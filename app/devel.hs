c1ef8a757e29e5318cd3e6289274949b531bead2
maxrosssp <maxrosssp>
2015-12-28 16:49:02 -0500

    first commit

---
+++ app/devel.hs
@@ -0,0 +1,22 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE PackageImports #-}
+
+module Main where
+
+--import "fileshare-final" Application (develMain)
+import Prelude (IO)
+
+import Control.Concurrent.STM
+import Data.IntMap
+import Yesod
+
+import Dispatch ()
+import Foundation
+
+main :: IO ()
+main = do
+    tstore <- atomically $ newTVar empty
+    tident <- atomically $ newTVar 0
+    warpEnv $ App tident tstore
